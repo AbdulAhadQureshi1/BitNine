faf3a3a2fe827609aa2e5013d49bca82e49af026
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2023-07-27 21:36:47 +0500

    Add Signup Functionailty with Backend

--- Question5-Part1/client/src/App.jsx
+++ Question5-Part1/client/src/App.jsx
@@ -1,9 +1,19 @@
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Signup />} index />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Question5-Part1/client/src/Components/Login.jsx
@@ -0,0 +1,81 @@
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+const Login = () => {
+    const email = useRef();
+    const pass = useRef();
+
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setLoading(true);
+
+
+        setLoading(false);
+    }
+
+  return (
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                  ref={email}
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                  ref={pass}
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full text-white bg-blue-500 bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              >
+                {loading ? 'Logging in...' : 'Log In'}
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don't have an account?{" "}
+                <Link
+                  to="/signup"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  SignUp here
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
